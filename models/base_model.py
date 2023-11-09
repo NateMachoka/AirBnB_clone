cb49bb4cf0c5ae3b4680fb145f30971ef56f012d
NateMachoka <natemachoka@gmail.com>
2023-11-08 22:35:07 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,23 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class for common attributes/methods."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(
+                            self,
+                            key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                            )
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
