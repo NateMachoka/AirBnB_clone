6d2cd52bf3b84d309f36d7151c8cc7180f8d8420
NateMachoka <natemachoka@gmail.com>
2023-11-08 21:04:22 -0800

    base_model.py

---
+++ README.md
@@ -0,0 +1,12 @@
+# AirBnB Clone - Console
+
+## Project Description
+Welcome to the AirBnB clone project! This project is the first step in building a fully functional AirBnB web application. The primary goal of this stage is to create a command-line interface that allows you to manage various objects related to AirBnB, such as users, states, cities, and places.
+
+## Command Interpreter Description
+
+### How to Start
+To start the AirBnB Console, simply run the `console.py` script. Ensure that you have Python 3.8.5 or later installed on your system.
+
+```bash
+$ ./console.py
---
+++ tests/test_base_model.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest.mock import patch
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.base_model, 'id'))
+        self.assertTrue(hasattr(self.base_model, 'created_at'))
+        self.assertTrue(hasattr(self.base_model, 'updated_at'))
+
+    def test_str_method(self):
+        expected_str = "[BaseModel] ({}) {}".format(self.base_model.id, self.base_model.__dict__)
+        self.assertEqual(str(self.base_model), expected_str)
+
+    @patch('models.base_model.datetime')
+    def test_save_method(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2022, 1, 1)
+        self.base_model.save()
+        self.assertEqual(self.base_model.updated_at, datetime(2022, 1, 1))
+
+    def test_to_dict_method(self):
+        obj_dict = self.base_model.to_dict()
+
+        self.assertTrue(isinstance(obj_dict, dict))
+        self.assertIn('__class__', obj_dict)
+        self.assertIn('id', obj_dict)
+        self.assertIn('created_at', obj_dict)
+        self.assertIn('updated_at', obj_dict)
+
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['id'], self.base_model.id)
+        self.assertEqual(obj_dict['created_at'], self.base_model.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], self.base_model.updated_at.isoformat())
+
+
+if __name__ == '__main__':
+    unittest.main()
