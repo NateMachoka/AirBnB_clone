fe15c2c5b3195280724282486bb40e17c6da6e43
NateMachoka <natemachoka@gmail.com>
2023-11-10 00:46:09 -0800

    console

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ Implements the command interpreter for the AirBnB clone project.
 """
 
 import cmd
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -24,6 +25,112 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program"""
         print()  # Print a new line before exiting
         return True
+
+    def emptyline(self):
+        """Do nothing on empty line (pressing ENTER)"""
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, saves it, and prints the id."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in storage.classes():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        obj_dict = storage.all().get(key)
+        if obj_dict is None:
+            print("** no instance found **")
+        else:
+            print(obj_dict)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in storage.classes():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        obj_dict = storage.all().get(key)
+        if obj_dict is None:
+            print("** no instance found **")
+        else:
+            del storage.all()[key]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances."""
+        objs = storage.all()
+        if not arg:
+            print([str(objs[key]) for key in objs])
+        else:
+            args = arg.split()
+            class_name = args[0]
+            if class_name not in storage.classes.keys():
+                print("** class doesn't exist **")
+            else:
+                print([str(objs[key]) for key in objs if key.startswith(class_name)])
+
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in storage.classes():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        obj_dict = storage.all().get(key)
+        if obj_dict is None:
+            print("** no instance found **")
+        else:
+            if len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            else:
+                attr_name = args[2]
+                attr_value = args[3]
+                setattr(obj_dict, attr_name, type(getattr(obj_dict, attr_name))(attr_value))
+                storage.save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,10 +13,15 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
+    __classes = {}
 
     def all(self):
         """Returns the dictionary __objects."""
         return self.__objects
+
+    def classes(self):
+        """Returns the dictionary __classes."""
+        return self.__classes
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
@@ -31,15 +36,14 @@ class FileStorage:
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
+        from models.base_model import BaseModel
+
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as file:
                 loaded_objects = json.load(file)
                 for key, value in loaded_objects.items():
                     class_name, obj_id = key.split('.')
-                    obj_cls = globals().get(class_name)
-                    if obj_cls:
-                        self.__objects[key] = obj_cls(**value)
-                    else:
-                        print(f"Warning: Class '{class_name}' not found.")
+                    obj_cls = globals()[class_name]
+                    self.__objects[key] = obj_cls(**value)
         except FileNotFoundError:
             pass
